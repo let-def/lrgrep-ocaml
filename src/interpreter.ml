f3457170a4cb9bc6a07ec33cb41960d60e84cf89
Frédéric Bour <frederic.bour@lakaban.net>
2025-01-17 17:02:01 +0900

    Port code from latest LRGrep

--- src/interpreter.ml
+++ src/interpreter.ml
@@ -1,5 +1,3 @@
-open Utils
-open Fix.Indexing
 
 (* The lexer generator. Command-line parsing. *)
 
@@ -53,46 +51,9 @@ let grammar_filename =
   filename
 
 module Grammar = MenhirSdk.Cmly_read.Read(struct let filename = grammar_filename end)
+module Interpreter = Lrgrep_interpreter.Make(Grammar)()
 
 let () = Sys.remove grammar_filename
-
-module Info = Mid.Info.Make(Grammar)
-module Viable = Mid.Viable_reductions.Make(Info)()
-module Regexp = Mid.Regexp.Make(Info)(Viable)
-
-let print_loc ((loc_start : Lexing.position), (loc_end : Lexing.position)) =
-    let sprintf = Printf.sprintf in
-    let sline = loc_start.pos_lnum in
-    let scol  = loc_start.pos_cnum - loc_start.pos_bol in
-    let eline = loc_end.pos_lnum in
-    let ecol  = loc_end.pos_cnum - loc_end.pos_bol in
-    if sline = eline then
-      sprintf "line %d:%d-%d\t" sline scol ecol
-    else
-      sprintf "from %d:%d to %d:%d\t" sline scol eline ecol
-
-let print_item (prod, pos) =
-  let open Info in
-  let rhs = Production.rhs prod in
-  let path = ref [] in
-  let add_dot i = if pos = i then path := "." :: !path in
-  add_dot (Array.length rhs);
-  for i = Array.length rhs - 1 downto 0 do
-    path := Info.Symbol.name rhs.(i) :: !path;
-    add_dot i;
-  done;
-  path := (Nonterminal.to_string (Production.lhs prod) ^ ":") :: !path;
-  String.concat " " !path
-
-let print_items indent suffix items =
-  Printf.printf "\t\t%s\x1b[0;32m[%s" (String.make indent ' ') suffix;
-  let pad = String.make (indent + 1 + String.length suffix) ' ' in
-  List.iteri (fun i item ->
-    if i > 0 then
-      Printf.printf "\n\t\t%s" pad;
-    Printf.printf " / %s" (print_item item);
-  ) items;
-  Printf.printf "]\n"
 
 let get_token lexbuf =
   match Lexer_raw.token lexbuf with
@@ -126,126 +87,43 @@ let do_parse
   | _ -> assert false
 
 
-let rec get_states acc env =
+let rec get_states initial_loc env () =
   let module I = Parser_raw.MenhirInterpreter in
-  let loc =
+  let state = Grammar.Lr1.of_int (I.current_state_number env) in
+  let start, stop =
     match I.top env with
-    | Some (I.Element (_,_,start,stop)) -> Some (start, stop)
-    | None -> None
+    | Some (I.Element (_,_,start,stop)) -> (start, stop)
+    | None -> (initial_loc, initial_loc)
   in
-  let lr1 = Index.of_int Info.Lr1.n (I.current_state_number env) in
-  let acc = (lr1, loc) :: acc in
-  match I.pop env with
-  | None -> acc
-  | Some env' -> get_states acc env'
+  Seq.Cons (
+    (state, start, stop),
+    match I.pop env with
+    | None -> Seq.empty
+    | Some env' -> get_states initial_loc env'
+  )
 
-let get_states env =
-  List.rev (get_states [] env)
-
-let print_lr1 state =
-  match Info.Lr1.incoming state with
-  | None -> None
-  | Some sym -> Some (Info.Symbol.name sym)
-
-open Info
-
-let rec display_steps la n acc = function
-    | [] -> acc
-    | {Viable. reachable=_; candidates} :: rest ->
-      let acc = List.fold_left (display_candidate la n) acc candidates in
-      display_steps la (n - 1) acc rest
-
-and display_candidate
-  : type a . Info.Terminal.set -> int -> _ -> a Viable.goto_candidate -> _ =
-  fun la n acc {Viable. target; lookahead; filter=_; reduction=_} ->
-  let la = IndexSet.inter la lookahead in
-  if IndexSet.is_empty la then
-    acc
-  else
-    let {Viable. inner; outer} = Viable.get_transitions target in
-    let acc =
-      if outer <> []
-      then (la, outer) :: acc
-      else acc
-    in
-    let acc = display_steps la (n + 1) acc inner in
-    let config = Viable.get_config target in
-    Printf.printf "\t\t%s\x1b[1;33m↱ %s\n"
-      (String.make n ' ') (Option.get (print_lr1 config.top));
-    if not !opt_no_reduce_filter then (
-      let suffix = match config.rest with
-        | [] | [_] -> [config.top]
-        | _ :: rest -> config.top :: rest
-      in
-      let suffix =
-        Utils.Misc.string_concat_map "; "
-          (fun lr1 -> Option.get (print_lr1 lr1))
-          suffix
-      in
-      print_items (n + 2) suffix (Lr1.items config.top);
-    );
-    acc
-
-let process_result lexbuf = function
+let process_result (lexbuf : Lexing.lexbuf) = function
   | None -> print_endline "Successful parse"
   | Some env ->
-    let stack = get_states env in
-    (*Format.printf "let stack = [%s]\n"
-      (String.concat ";" (List.map string_of_int (List.map fst stack)));*)
-    Format.printf "%a, parser stack (most recent first):\n%!"
-      Location.print_loc (Location.curr lexbuf);
-    let outer = ref [] in
-    List.iteri (fun i (state, loc) ->
-        if i = 0 then (
-          let top, _ = List.hd stack in
-          outer := [Terminal.all, Vector.get Viable.initial top]
-        );
-        let rec process_steps acc = function
-          | (_, []) -> acc
-          | (la, step :: next) ->
-            let candidates =
-              List.filter
-                (fun c -> IndexSet.mem state c.Viable.filter)
-                step.Viable.candidates
-            in
-            let threads = List.fold_left (display_candidate la 1) [] candidates in
-            (la, next) :: process_threads acc threads
-        and process_threads acc = function
-          | [] -> acc
-          | thread :: threads ->
-            process_threads (process_steps acc thread) threads
-        in
-        outer := process_threads [] !outer;
-        let items = Info.Lr1.items state in
-        if (i = 0 && not !opt_no_reduce_filter) then (
-          print_items 0 "" items;
-        ) else if !opt_stack_items then (
-          print_string "\x1b[0;36m";
-          List.iter
-            (fun item -> print_endline ("\t\t  [" ^ print_item item ^ "]"))
-            items;
-        );
-        print_string "\x1b[0m- ";
-        print_string (
-          match loc with
-          | None -> "entrypoint\t"
-          | Some loc -> print_loc loc
-        );
-        print_string "\x1b[1m";
-        begin match print_lr1 state with
-          | None ->
-            let find_state (_,_,state') = state' = Info.Lr1.to_g state in
-            let nt, _prod, _ = List.find find_state Grammar.Grammar.entry_points in
-            print_endline (Grammar.Nonterminal.name nt)
-          | Some sym -> print_endline sym
-        end;
-        print_string "\x1b[0m";
-      ) stack;
+    let initial_loc = {
+      Lexing.
+      pos_fname = lexbuf.lex_start_p.pos_fname;
+      pos_lnum = 1;
+      pos_bol = 0;
+      pos_cnum = 0;
+    } in
+    let config = {
+      Lrgrep_interpreter.default_config with
+      print_reduce_filter = not !opt_no_reduce_filter;
+      print_stack_items = !opt_stack_items;
+    } [@ocaml.warning "w-23"] in
+    let stack = get_states initial_loc env in
+    Interpreter.analyze_stack config stack;
     if !opt_dump_states then
       Printf.printf "states = %s\n"
         (String.concat ","
-           (List.map (fun (idx, _) -> string_of_int (idx : _ index :> int))
-              stack))
+           (List.map (fun (idx, _, _) -> string_of_int (Grammar.Lr1.to_int idx))
+              (List.of_seq stack)))
 
 let () =
   match !opt_infile with
